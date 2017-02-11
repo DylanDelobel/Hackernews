73e38cc3aedf2ecbc5b5a20ae4382eed1f07f763
Dylan Delobel <dylan.delobel66@gmail.com>
2017-02-11 23:01:06 +0100

    Destructuring list & search

--- src/App.js
+++ src/App.js
@@ -47,12 +47,13 @@ class App extends Component {
     }
 
     render() {
+        const { searchTerm, list } = this.state;
         return (
             <div className="App">
                 <form>
-                    <input type="text" onChange={this.onSearchChange} />
+                    <input type="text" value={searchTerm} onChange={this.onSearchChange} />
                 </form>
-                { this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+                { list.filter(isSearched(searchTerm)).map(item =>
                     <div key={item.objectID}>
                         <span>
                             <a href={item.url}>{item.title}</a>
